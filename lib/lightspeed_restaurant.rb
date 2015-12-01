34f25dedab1df3b0b7eb02960382ffc46b90af04
Olivier Buffon <olivier@chronogolf.ca>
2015-12-01 11:03:33 -0500

    Add required resources to lightspeed_restaurant parent class

--- lib/lightspeed_restaurant.rb
+++ lib/lightspeed_restaurant.rb
@@ -1,11 +1,21 @@
 #   Lightspeed Restaurant Ruby Bindings
 #   API spec at http://staging-exact-integration.posios.com/PosServer/swagger-ui.html
 
+require 'rest-client'
+require 'json'
+
+
 #
 #   Version
 #
 
-require "lightspeed_restaurant/version"
+require 'lightspeed_restaurant/version'
+
+
+#
+#   Resources
+#
+
 require 'lightspeed_restaurant/account'
 require 'lightspeed_restaurant/authentication'
 require 'lightspeed_restaurant/core'
@@ -14,6 +24,15 @@ require 'lightspeed_restaurant/inventory'
 require 'lightspeed_restaurant/online_order'
 require 'lightspeed_restaurant/registration'
 require 'lightspeed_restaurant/reservation'
+
+
+#
+#   Errors
+#
+
+require 'lightspeed_restaurant/errors/lightspeed_restaurant_error'
+require 'lightspeed_restaurant/errors/api_connection_error'
+
 
 module LightspeedRestaurant
   # Your code goes here...
