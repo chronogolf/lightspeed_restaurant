cafa21bd6ca4c2f2652fee0ad7ede21ae2384896
Olivier Buffon <olivier@chronogolf.ca>
2015-12-03 15:28:21 -0500

    Remove unnecessary require in the main module

--- lib/lightspeed_restaurant.rb
+++ lib/lightspeed_restaurant.rb
@@ -1,43 +1,11 @@
 #   Lightspeed Restaurant Ruby Bindings
 #   API spec at http://staging-exact-integration.posios.com/PosServer/swagger-ui.html
 
-require 'rest-client'
+require 'excon'
 require 'json'
 
-#
-#   Version
-#
-
 require 'lightspeed_restaurant/version'
-
-#
-#   Resources
-#
-
-require 'lightspeed_restaurant/company'
-require 'lightspeed_restaurant/customer'
-require 'lightspeed_restaurant/establishment'
-require 'lightspeed_restaurant/feature'
-require 'lightspeed_restaurant/floor'
-require 'lightspeed_restaurant/order'
-require 'lightspeed_restaurant/payment_type'
-require 'lightspeed_restaurant/product'
-require 'lightspeed_restaurant/product_group'
-require 'lightspeed_restaurant/receipt'
-require 'lightspeed_restaurant/referral'
-require 'lightspeed_restaurant/report'
-require 'lightspeed_restaurant/reservation'
-require 'lightspeed_restaurant/table'
-require 'lightspeed_restaurant/tax'
-require 'lightspeed_restaurant/ticket'
-
-#
-#   Errors
-#
-
-require 'lightspeed_restaurant/errors/lightspeed_restaurant_error'
-require 'lightspeed_restaurant/errors/api_connection_error'
+require 'lightspeed_restaurant/client'
 
 module LightspeedRestaurant
-  @base_uri = 'http://us1.posios.com/PosServer'
 end
