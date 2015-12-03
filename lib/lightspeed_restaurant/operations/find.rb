b3f15ac9a3dafda98ac9e761ae7336aa8df65d62
Olivier Buffon <olivier@chronogolf.ca>
2015-12-03 15:33:04 -0500

    Create find operation module

---
+++ lib/lightspeed_restaurant/operations/find.rb
@@ -0,0 +1,10 @@
+module LightspeedRestaurant
+  module Operations
+    module Find
+      def find(id)
+        response = client.get(self.class.resource_path + "/#{id}")
+        LightspeedRestaurant.const_get(self.class.resource_name).new(response)
+      end
+    end
+  end
+end
