729186775b261a86095b65e2034a0da7a16c18c5
Olivier Buffon <olivier@chronogolf.ca>
2015-12-04 17:02:11 -0500

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'lightspeed_restaurant'
+require 'webmock/rspec'
+require 'vcr'
+
+Dir[File.expand_path('support/**/*.rb', File.dirname(__FILE__))].each do |f|
+  require f
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
