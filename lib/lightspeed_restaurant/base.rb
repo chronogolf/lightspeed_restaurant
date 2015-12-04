e5f106d91448bf0159b32e26395712d1d670e129
Olivier Buffon <olivier@chronogolf.ca>
2015-12-04 11:18:13 -0500

    Improve hash and json generator method

--- lib/lightspeed_restaurant/base.rb
+++ lib/lightspeed_restaurant/base.rb
@@ -1,10 +1,21 @@
 module LightspeedRestaurant
   class Base
     def initialize(data = {})
-      data.each do |k, v|
-        v = nil if ['N/A', ''].include?(v)
-        send("#{k}=", v)
+      self.class.attributes.each do |attribute|
+        data  = data.each_with_object({}) { |(k, v), h| h[k.to_sym] = v }
+        value = ['N/A', ''].include?(data[attribute]) ? nil : data[attribute]
+        send("#{attribute}=", value)
       end
+    end
+
+    def to_hash
+      self.class.attributes.each_with_object({}) do |attribute, h|
+        h[attribute] = instance_variable_get("@#{attribute}")
+      end
+    end
+
+    def to_json
+      to_hash.to_json
     end
   end
 end
