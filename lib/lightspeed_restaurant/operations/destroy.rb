7793e0bb71bb1b09513a8e4aee03c54cd1d09a37
Olivier Buffon <olivier@chronogolf.ca>
2015-12-04 11:19:36 -0500

    Create destroy operation module

--- lib/lightspeed_restaurant/operations/destroy.rb
+++ lib/lightspeed_restaurant/operations/destroy.rb
@@ -1,6 +1,10 @@
 module LightspeedRestaurant
   module Operations
     module Destroy
+      def destroy(id)
+        response = JSON.parse(client.delete(self.class.resource_path + "/#{id}"))
+        LightspeedRestaurant.const_get(self.class.resource_name).new(response)
+      end
     end
   end
 end
