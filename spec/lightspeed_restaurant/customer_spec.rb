da5a6473c6cc9098468a1cb6892d792d5b9fc9bb
Olivier Buffon <olivier@chronogolf.ca>
2015-12-04 17:04:40 -0500

    Fix syntax

--- spec/lightspeed_restaurant/customer_spec.rb
+++ spec/lightspeed_restaurant/customer_spec.rb
@@ -6,8 +6,8 @@ module LightspeedRestaurant
 
     subject { LightspeedRestaurant::Customer }
 
-    it "can fetch customers" do
-      VCR.use_cassette("customers/list") do
+    it 'can fetch customers' do
+      VCR.use_cassette('customers/list') do
         customers = subject.all
         expect(customers).to be_an(Array)
         expect(customers.count).to eq(4)
@@ -18,50 +18,50 @@ module LightspeedRestaurant
       end
     end
 
-    it "can fetch an customer by ID" do
-      VCR.use_cassette("customers/find") do
+    it 'can fetch an customer by ID' do
+      VCR.use_cassette('customers/find') do
         customer = subject.find(2366)
         expect(customer).to be_a(LightspeedRestaurant::Customer)
       end
     end
 
-    context "creating" do
-      it "with valid information" do
-        VCR.use_cassette("customers/create") do
-          customer = subject.create(firstName: 'test5', lastName: 'test5', email: "test5@test.com")
+    context 'creating' do
+      it 'with valid information' do
+        VCR.use_cassette('customers/create') do
+          customer = subject.create(firstName: 'test5', lastName: 'test5', email: 'test5@test.com')
           expect(customer).to be_a(LightspeedRestaurant::Customer)
           expect(customer.id).not_to be_nil
         end
       end
 
-      it "missing a email" do
-        VCR.use_cassette("customers/create_invalid") do
+      it 'missing a email' do
+        VCR.use_cassette('customers/create_invalid') do
           expect do
-            subject.create(email: "")
+            subject.create(email: '')
           end.to raise_error(LightspeedRestaurant::APIError)
         end
       end
     end
 
-    context "updating" do
-      it "with valid information" do
-        VCR.use_cassette("customers/find") do
+    context 'updating' do
+      it 'with valid information' do
+        VCR.use_cassette('customers/find') do
           @customer = subject.find(2366)
         end
-        VCR.use_cassette("customers/update") do
-          @customer.email = "test-update@test.com"
+        VCR.use_cassette('customers/update') do
+          @customer.email = 'test-update@test.com'
           @customer = @customer.save
-          expect(@customer.email).to eq("test-update@test.com")
+          expect(@customer.email).to eq('test-update@test.com')
         end
       end
 
-      it "missing a email" do
-        VCR.use_cassette("customers/find") do
+      it 'missing a email' do
+        VCR.use_cassette('customers/find') do
           @customer = subject.find(2366)
         end
-        VCR.use_cassette("customers/update_invalid") do
+        VCR.use_cassette('customers/update_invalid') do
           expect do
-            @customer.email = ""
+            @customer.email = ''
             @customer.save
           end.to raise_error(LightspeedRestaurant::APIError)
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,6 @@ Dir[File.expand_path('support/**/*.rb', File.dirname(__FILE__))].each do |f|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
