b578e573052bfea641c125206bf18198be8b46ec
Olivier Buffon <olivier@chronogolf.ca>
2015-12-05 21:22:44 -0500

    Create shared example for find operation

---
+++ spec/support/find_spec_helper.rb
@@ -0,0 +1,11 @@
+shared_examples "a find operation" do
+
+  let(:resource_name) { described_class.to_s.gsub(/^.*::/, '').downcase }
+
+  it "can fetch a specific resource's occurrence by ID" do
+    VCR.use_cassette("#{resource_name}/find") do
+      resource = described_class.find(resource_id)
+      expect(resource).to be_a(described_class)
+    end
+  end
+end
