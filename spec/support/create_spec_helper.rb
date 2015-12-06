cbf0c5ad7de46b0ee8fba4bf9e330006d458378f
Olivier Buffon <olivier@chronogolf.ca>
2015-12-05 21:22:34 -0500

    Create shared example for create operation

---
+++ spec/support/create_spec_helper.rb
@@ -0,0 +1,20 @@
+shared_examples "a create operation" do
+
+  let(:resource_name) { described_class.to_s.gsub(/^.*::/, '').downcase }
+
+  it 'with valid params' do
+    VCR.use_cassette("#{resource_name}/create") do
+      resource = described_class.create(valid_params)
+      expect(resource).to be_a(described_class)
+      expect(resource.id).not_to be_nil
+    end
+  end
+
+  it 'with invalid params' do
+    VCR.use_cassette("#{resource_name}/create_invalid") do
+      expect do
+        described_class.create(invalid_params)
+      end.to raise_error(LightspeedRestaurant::APIError)
+    end
+  end
+end
