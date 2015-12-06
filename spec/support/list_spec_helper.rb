b897665c319c37e38c14a947f69b90235e339782
Olivier Buffon <olivier@chronogolf.ca>
2015-12-05 21:22:56 -0500

    Create shared example for list operation

---
+++ spec/support/list_spec_helper.rb
@@ -0,0 +1,16 @@
+shared_examples "a list operation" do
+
+  let(:resource_name) { described_class.to_s.gsub(/^.*::/, '').downcase }
+
+  it "can fetch all resource's occurrences" do
+    VCR.use_cassette("#{resource_name}/list") do
+      resources = described_class.all
+      expect(resources).to be_an(Array)
+      expect(resources.count).to eq(results_count)
+
+      resource = resources.first
+      expect(resource).to be_a(described_class)
+      expect(resource.id).to eq(resource_id)
+    end
+  end
+end
