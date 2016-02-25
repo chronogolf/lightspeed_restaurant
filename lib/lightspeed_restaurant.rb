6e2f553802075da468b37d6fdc116fd4aa15417c
Olivier Buffon <olivier@chronogolf.ca>
2016-02-25 16:03:03 -0500

    Code cleaning

--- lib/lightspeed_restaurant.rb
+++ lib/lightspeed_restaurant.rb
@@ -8,30 +8,31 @@ require 'lightspeed_restaurant/version'
 require 'lightspeed_restaurant/request'
 
 require 'lightspeed_restaurant/customer'
+require 'lightspeed_restaurant/customer_credit_change'
 require 'lightspeed_restaurant/receipt'
 
 module LightspeedRestaurant
   class << self
     attr_accessor :api_token, :base_url
-  end
 
-  def self.get(path, body = {}, query = {})
-    request(path, body, query).perform(method: :get)
-  end
+    def get(path, body = {}, query = {})
+      request(path, body, query).perform(method: :get)
+    end
 
-  def self.post(path, body = {}, query = {})
-    request(path, body, query).perform(method: :post)
-  end
+    def post(path, body = {}, query = {})
+      request(path, body, query).perform(method: :post)
+    end
 
-  def self.put(path, body = {}, query = {})
-    request(path, body, query).perform(method: :put)
-  end
+    def put(path, body = {}, query = {})
+      request(path, body, query).perform(method: :put)
+    end
 
-  def self.delete(path, body = {}, query = {})
-    request(path, body, query).perform(method: :delete)
-  end
+    def delete(path, body = {}, query = {})
+      request(path, body, query).perform(method: :delete)
+    end
 
-  def self.request(path, body, query)
-    LightspeedRestaurant::Request.new(@base_url, path, @api_token, body, query)
+    def request(path, body, query)
+      Request.new(@base_url, path, @api_token, body, query)
+    end
   end
 end
