82757f9ae45685a62607952ca21d9310e10937a1
Laurent Cobos <laurent@11factory.fr>
2018-02-24 10:05:04 -0500

    Handle LS resto empty body response after update

--- lib/lightspeed_restaurant/operations/update.rb
+++ lib/lightspeed_restaurant/operations/update.rb
@@ -3,19 +3,8 @@ module LightspeedRestaurantClient
     module Update
       def update(id, attributes)
         updated_object = new(attributes)
-        response = LightspeedRestaurantClient.put(resource_path + "/#{id}", updated_object)
-        new(build_payload(attributes, response))
-      end
-
-      private
-
-      def build_payload(attributes, response)
-        begin
-          return JSON.parse(response)
-        rescue JSON::ParserError => error
-          raise error if response.to_i.zero?
-        end
-        attributes.merge!(id: response.to_i)
+        LightspeedRestaurantClient.put(resource_path + "/#{id}", updated_object)
+        updated_object
       end
     end
   end
