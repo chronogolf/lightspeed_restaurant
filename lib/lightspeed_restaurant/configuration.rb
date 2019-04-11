d46da7f803ce134f6ebf5ee9859a9e796986732e
Laurent Cobos <laurent@11factory.fr>
2019-04-11 14:44:27 +0200

    ruby 2.2.1 comp

--- lib/lightspeed_restaurant/configuration.rb
+++ lib/lightspeed_restaurant/configuration.rb
@@ -10,7 +10,10 @@ module LightspeedRestaurantClient
     end
 
     def with(attributes)
-      attributes_with_string_keys = attributes.transform_keys(&:to_s)
+      attributes_with_string_keys = {}
+      attributes.dup.each_pair do |name, value|
+        attributes_with_string_keys[name.to_s] = value
+      end
       self.class.new(
         attributes_with_string_keys.fetch('api_token', api_token),
         attributes_with_string_keys.fetch('base_uri', base_uri)
