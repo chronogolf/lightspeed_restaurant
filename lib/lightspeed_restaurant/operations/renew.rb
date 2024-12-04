628fda0d15328b8e958e4c6b24378dbe5c4edbf5
Chen Zhang <chen.zhang@lightspeedhq.com>
2024-12-04 13:34:52 -0500

    Add webhook support to LightspeedRestaurantClient

---
+++ lib/lightspeed_restaurant/operations/renew.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module LightspeedRestaurantClient
+  module Operations
+    module Renew
+      def renew(id, configuration = nil)
+        response = JSON.parse(LightspeedRestaurantClient.post(default_resource_path + "/#{id}" + "/renew", {}, configuration))
+        new(response)
+      end
+    end
+  end
+end
