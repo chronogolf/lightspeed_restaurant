4086e731611d73235b6e6143684783bbc54bb510
Haani Al-Chalabi <haani.alchalabi@lightspeedhq.com>
2025-09-16 15:51:13 -0400

    add update

--- spec/lightspeed_restaurant/external_payment_provider_spec.rb
+++ spec/lightspeed_restaurant/external_payment_provider_spec.rb
@@ -61,5 +61,18 @@ module LightspeedRestaurantClient
         end
       end
     end
+
+    context 'when updating' do
+      let(:resource_id) { 1 }
+
+      around do |test|
+        VCR.use_cassette("#{resource_name}/update", allow_playback_repeats: true) { test.run }
+      end
+
+      it "updates" do
+        resource = described_class.update(resource_id, {username: "updated_name"})
+        expect(resource.username).to eq "updated_name"
+      end
+    end
   end
 end
